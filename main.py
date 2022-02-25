6547ad0376a5c6511b036884f080924dc0edf0bf
Bishneet Singh <36583058+singhb2020@users.noreply.github.com>
2022-02-25 18:57:01 -0500

    Kivy App Trial

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 # Bishneet Singh
 
 # ------------------ Importing Libraries ------------------ #
+import imp
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
 
 # ------------------ Importing Functions ------------------ #
@@ -10,10 +20,24 @@ from database import create_table, insert_data, retrieve_data
 
 # ------------------ Main Functions ------------------ #
 
-
-def main():
+class FirstWindow(Screen):
     pass
 
-    
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class mainApp(App):
+    def build(self):
+        kv = Builder.load_file('main.kv')
+        return kv
+
 if __name__ == '__main__':
-    main()
+    mainApp().run()
